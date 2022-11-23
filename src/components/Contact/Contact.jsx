61e8b511526dddc70c25945b42c9c21888c3b24d
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-23 17:39:58 +0100

    se agregó useEffect como validación al imput name

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Contact.css";
 import { useNavigate } from "react-router-dom";
 
@@ -6,6 +6,7 @@ const Contact = () => {
   let navigate = useNavigate();
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [visible, setVisible] = useState(true);
   const [data, setData] = useState({
     name: "",
     email: "",
@@ -20,14 +21,24 @@ const Contact = () => {
     setData({ ...initialState });
   };
 
-  const handleInputChange = (event) => {
-    if (data.name.length + 1 < 3) {
+  useEffect(() => {
+    if (data.name.length < 3) {
       setMessage("Tu nombre debe tener al menos 3 caracteres");
       setBtnDisabled(true);
     } else {
       setMessage(null);
       setBtnDisabled(false);
     }
+  }, [data]);
+
+  const handleInputChange = (event) => {
+    // if (data.name.length + 1 < 3) {
+    //   setMessage("Tu nombre debe tener al menos 3 caracteres");
+    //   setBtnDisabled(true);
+    // } else {
+    //   setMessage(null);
+    //   setBtnDisabled(false);
+    // }
 
     setData({
       ...data,
@@ -43,6 +54,7 @@ const Contact = () => {
     setTimeout(() => {
       navigate("/");
     }, 1000);
+    setVisible(false);
   };
 
   return (
@@ -69,7 +81,7 @@ const Contact = () => {
               Enviar
             </button>
           </form>
-          {message}
+          <p>{visible ? message : "Redireccionandote al Home..."}</p>
           <iframe
             title="Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d197293.724019207!2d-0.5019392151834063!3d39.40796612298359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd604f4cf0efb06f%3A0xb4a351011f7f1d39!2sValencia!5e0!3m2!1ses-419!2ses!4v1669209549068!5m2!1ses-419!2ses"
